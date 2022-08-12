bf6f5c44adb418f8eb23f3ecda30949504746427
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-12 03:36:55 +0100

    Installed some fewer packages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
  const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='p-5 pb-0 flex 
+    flex-wrap sm:justify-between 
+    items-center justify-center border-b dark:border-neutral-700 border-neutral-200'>
+      <div className = "flex justify-between items-center space-x-5 w-screen"></div>
+    </div>
   )
 }
 
